932dee775c9d414074df83f06602c693f6d09ed4
mariaeugenio <mecmaria95@gmail.com>
2016-05-01 15:36:09 +0200

    Tema 10: Lista de preguntas

--- routes/index.js
+++ routes/index.js
@@ -8,8 +8,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
-router.get('/question', quizController.question);
-router.get('/check', quizController.check);
+router.get('/quizzes', quizController.index);
+router.get('/quizzes/:quizId(\\d+)', quizController.show);
+router.get('/quizzes/:quizId(\\d+)/check', quizController.check);
 
 /* GET author page. */
 router.get('/author', function(req, res, next) {
